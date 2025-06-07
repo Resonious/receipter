acae4cd41f9546008939c6831ed92c2c427ba627
Nigel Baillie <nigel@baillie.dev>
2025-06-08 04:17:11 +0900

    messin around

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import PostalMime from "postal-mime";
+import OpenAI from "openai";
+import { z } from "zod";
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
-    return new Response('Hello World!');
+    return new Response("Hello World!");
+  },
+
+  async email(message, env, ctx) {
+    const email = await PostalMime.parse(message.raw, {
+      attachmentEncoding: "arraybuffer",
+    });
   },
 } satisfies ExportedHandler<Env>;
